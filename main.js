4e68071847fb9b9363724aa5138676c0593f308b
akash singh <akash9936@gmail.com>
2023-09-13 18:53:28 +0000

    display data in UI

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const projectButton = document.getElementById('projectButton');
     const projectDataDiv = document.getElementById('projectData');
+    const dataTableBody = document.querySelector('#dataTable tbody');
 
     projectButton.addEventListener('click', () => {
         // Make an HTTP GET request to your Node.js server
-     //   const baseUrl= process.env.NODE_ENV=="  " ? "" : 'http://localhost:3000/getProjectData';
         fetch('http://localhost:3000/getProjectData') // Assuming this route is defined in your server.js
             .then(response => response.json())
-            .then(data => { 
+            .then(data => {
                 // Handle the response data
-                displayProjectData(data, projectDataDiv);
+                displayProjectDataV2(data, dataTableBody);
             })
             .catch(error => {
                 // Handle errors
@@ -19,16 +17,44 @@ document.addEventListener('DOMContentLoaded', () => {
             });
     });
 
-    // Function to display project data in the UI
-    // Function to display project data in the UI as a bar chart
-    function displayProjectData(data, targetElement) {
-        targetElement.innerHTML = '';
 
-        const preElement = document.createElement('pre');
-        preElement.textContent = JSON.stringify(data, null, 2);
-
-        // Append the <pre> element to the target
-        targetElement.appendChild(preElement);
-
+    function displayProjectDataV2(data, dataTableBody) {
+        // Clear the existing table rows
+        dataTableBody.innerHTML = '';
+    
+        // Create the table headers (thead)
+        const thead = document.createElement('thead');
+        const headerRow = document.createElement('tr');
+    
+        // Create table header cells for each field
+        data.field.forEach(field => {
+            const headerCell = document.createElement('th');
+            headerCell.textContent = field.name;
+            headerRow.appendChild(headerCell);
+        });
+    
+        thead.appendChild(headerRow);
+    
+        // Append the thead to the table
+        dataTableBody.appendChild(thead);
+    
+        // Create the table body (tbody) and populate it with data
+        const tbody = document.createElement('tbody');
+        data.records.forEach(record => {
+            const row = document.createElement('tr');
+    
+            // Create table cells for each field
+            data.field.forEach(field => {
+                const cell = document.createElement('td');
+                cell.textContent = record[field.id];
+                row.appendChild(cell);
+            });
+    
+            tbody.appendChild(row);
+        });
+    
+        // Append the tbody to the table
+        dataTableBody.appendChild(tbody);
     }
+    
 });
